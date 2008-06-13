226fbece3b7c9a5e4846bd3f236878569f4d1a19
Michael K. Johnson <http://issues.rpath.com/>
2008-06-13 17:45:17 -0400

    fix epydoc formatting

--- rbuild/facade/conaryfacade.py
+++ rbuild/facade/conaryfacade.py
@@ -285,10 +285,10 @@ class ConaryFacade(object):
         as a list of C{(I{status}, I{filename})} tuples where
         C{I{status}} is a single character describing the status
         of the file C{I{filename}}:
-        - C{?}: File not managed by Conary
-        - C{A}: File added since last commit (or since package created if no commit)
-        - C{M}: File modified since last commit
-        - C{R}: File removed since last commit
+         - C{?}: File not managed by Conary
+         - C{A}: File added since last commit (or since package created if no commit)
+         - C{M}: File modified since last commit
+         - C{R}: File removed since last commit
         @param targetDir: name of directory for which to fetch status.
         @type targetDir: string
         @rtype: list
