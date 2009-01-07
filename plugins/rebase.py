00d507bbdaa8240c57e099a51852b488aab793f3
Michael K. Johnson <http://issues.rpath.com/>
2009-01-07 17:32:42 -0500

    rebase handles out-of-band product-definition modification better, no longer announces no new version as an update to "None" (RBLD-164)

--- plugins/rebase.py
+++ plugins/rebase.py
@@ -14,8 +14,23 @@
 """
 rebase command and related utilities.
 """
+import os
+
 from rbuild.pluginapi import command
 from rbuild import pluginapi
+
+from rbuild import errors
+
+class ModifiedFilesError(errors.PluginError):
+    template = ('Modifed files %(filenames)r exist in product checkout'
+                ' directory %(dirname)r')
+    params = ['filenames', 'dirname']
+
+class FileConflictsError(errors.PluginError):
+    template = ('Failed merge files %(filenames)r exist in product checkout'
+                ' directory %(dirname)r')
+    params = ['filenames', 'dirname']
+
 
 class RebaseCommand(command.BaseCommand):
     help = 'Update product to most recent platform version'
@@ -39,11 +54,18 @@ class Rebase(pluginapi.Plugin):
 
     def rebaseProduct(self, label=None):
         handle = self.handle
+        ui = handle.ui
+
+        proddir = handle.productStore.getProductDefinitionDirectory()
         conaryClient = handle.facade.conary._getConaryClient()
-        # First, update to latest upstream to avoid regressions (RBLD-155)
+        self._raiseErrorIfModified(proddir)
+        self._raiseErrorIfConflicts(proddir)
+        # update to latest upstream to avoid regressions (RBLD-155)
         handle.productStore.update()
+        self._raiseErrorIfConflicts(proddir)
         oldPlatformSource = handle.product.getPlatformSourceTrove()
         handle.product.rebase(conaryClient, label=label)
+        self._raiseErrorIfConflicts(proddir)
         handle.product.saveToRepository(conaryClient)
         handle.productStore.update()
         platformSource = handle.product.getPlatformSourceTrove()
@@ -53,6 +75,44 @@ class Rebase(pluginapi.Plugin):
             for i in range(len(b)):
                 if a[i] != b[i]:
                     return '/'.join(b[i:])
-        handle.ui.info(
-            'Updated from %s to latest %s' % (oldPlatformSource,
-                trailingVersionDifference(oldPlatformSource, platformSource,)))
+        if oldPlatformSource != platformSource:
+            ui.info(
+                'Updated from %s to latest %s' % (oldPlatformSource,
+                    trailingVersionDifference(oldPlatformSource,
+                                              platformSource,)))
+
+    def _raiseErrorIfModified(self, proddir):
+        '''
+        Enforce preconditions for safe rebasing: no files in checkout modified
+        @param proddir: path to product checkout directory
+        '''
+        modifiedFiles = self._modifiedFileNames(proddir)
+        if modifiedFiles:
+            raise ModifiedFilesError(filenames=', '.join(modifiedFiles),
+                                     dirname=proddir)
+    def _raiseErrorIfConflicts(self, proddir):
+        '''
+        Enforce preconditions for safe rebasing: no .conflicts files exist
+        @param proddir: path to product checkout directory
+        '''
+        conflictFiles = self._fileConflictNames(proddir)
+        if conflictFiles:
+            raise FileConflictsError(filenames=', '.join(conflictFiles),
+                                     dirname=proddir)
+
+    def _modifiedFileNames(self, checkoutDirectory):
+        '''
+        Report any modified files in a checkout
+        @param checkoutDirectory: path to directory to test
+        '''
+        conaryfacade = self.handle.facade.conary
+        return [x[1] for x in conaryfacade.getCheckoutStatus(checkoutDirectory)
+                if x[0] == 'M']
+
+    def _fileConflictNames(self, checkoutDirectory):
+        '''
+        Tests whether .conflicts files exist in a checkout
+        @param checkoutDirectory: path to directory to test
+        '''
+        return [x for x in os.listdir(checkoutDirectory)
+                if x.endswith('.conflicts')]
