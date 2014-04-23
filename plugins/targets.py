b5801b6b0e9e32b7c5482086b5d5483626d7846a
Walter Scheper <Walter.Scheper@sas.com>
2014-04-23 13:55:09 -0400

    Plugin cleanup
    Refactor target creation code into targets plugin.

--- plugins/targets.py
+++ plugins/targets.py
@@ -22,6 +22,43 @@ from rbuild import pluginapi
 from rbuild.pluginapi import command
 
 
+class CreateTargetCommand(command.BaseCommand):
+    help = 'Create a target on a SAS App Engine'
+    paramHelp = '<TYPE>'
+    docs = {'list': 'List available target types',
+            'from-file': 'Load config from file',
+            'to-file': 'Write config to file',
+            }
+
+    def addLocalParameters(self, argDef):
+        argDef['list'] = '-l', command.NO_PARAM
+        argDef['from-file'] = '-f', command.ONE_PARAM
+        argDef['to-file'] = '-o', command.ONE_PARAM
+
+    def runCommand(self, handle, argSet, args):
+        ui = handle.ui
+        rb = handle.facade.rbuilder
+
+        list_types = argSet.pop('list', False)
+        fromFile = argSet.pop('from-file', None)
+        toFile = argSet.pop('to-file', None)
+
+        if list_types:
+            types = rb.getTargetTypes().keys()
+            ui.write("Available target types: %s" % ', '.join(types))
+            return
+
+        if fromFile:
+            handle.DescriptorConfig.readConfig(fromFile)
+
+        _, targetType = self.requireParameters(args, expected=['TYPE'])
+        target = handle.Targets.createTarget(targetType)
+        handle.Targets.configureTargetCredentials(target)
+
+        if toFile:
+            handle.DescriptorConfig.writeConfig(toFile)
+
+
 class ListTargetsCommand(command.ListCommand):
     help = 'list targets'
     resource = 'targets'
@@ -36,8 +73,51 @@ class Targets(pluginapi.Plugin):
     name = 'targets'
 
     def initialize(self):
+        self.handle.Commands.getCommandClass('create').registerSubCommand(
+            'target', CreateTargetCommand)
         self.handle.Commands.getCommandClass('list').registerSubCommand(
             'targets', ListTargetsCommand)
+
+    def createTarget(self, targetType):
+        '''
+            Create a target
+
+            @param targetType: type of target to create
+            @type targetType: str
+            @return: configured target
+            @rtype: rObj(target)
+        '''
+        dc = self.handle.DescriptorConfig
+        rb = self.handle.facade.rbuilder
+
+        descriptor_xml = rb.getTargetDescriptor(targetType)
+        if descriptor_xml is None:
+            raise errors.PluginError('No such target type: %s' % targetType)
+
+        ddata = dc.createDescriptorData(fromStream=descriptor_xml)
+
+        target = rb.createTarget(ddata, targetType)
+        rb.configureTarget(target, ddata)
+        return target
+
+    def configureTargetCredentials(self, target):
+        '''
+            Set credentials on a target
+
+            @param target: target to set credentials for
+            @type target: rObj(target)
+            @return: target with credentials Set
+            @rtype: rObj(target)
+        '''
+        dc = self.handle.DescriptorConfig
+        rb = self.handle.facade.rbuilder
+
+        creds_ddata = dc.createDescriptorData(
+            fromStream=target.actions[1].descriptor)
+        try:
+            rb.configureTargetCredentials(target, creds_ddata)
+        except errors.RbuildError as e:
+            self.handle.ui.warning(str(e))
 
     def list(self, *args, **kwargs):
         return self.handle.facade.rbuilder.getTargets()
--- rbuild_test/unit_test/pluginstest/targetstest.py
+++ rbuild_test/unit_test/pluginstest/targetstest.py
@@ -21,12 +21,86 @@ from testutils import mock
 from rbuild_test import rbuildhelp
 
 
-class ListTargetsTest(rbuildhelp.RbuildHelper):
+class AbstractTargetTest(rbuildhelp.RbuildHelper):
+    def setUp(self):
+        rbuildhelp.RbuildHelper.setUp(self)
+        self.handle = self.getRbuildHandle(mock.MockObject())
+        self.handle.Create.registerCommands()
+        self.handle.List.registerCommands()
+        self.handle.Targets.registerCommands()
+        self.handle.Create.initialize()
+        self.handle.List.initialize()
+        self.handle.Targets.initialize()
+
+
+class CreateTargetTest(AbstractTargetTest):
+    def testCreateTargetArgParse(self):
+        self.checkRbuild(
+            'create target --list --from-file=file --to-file=toFile vmware',
+            'rbuild_plugins.targets.CreateTargetCommand.runCommand',
+            [None, None, {
+                'list': True,
+                'from-file': 'file',
+                'to-file': 'toFile',
+                }, ['create', 'target', 'vmware']])
+
+    def testCreateTargetCmdline(self):
+        handle = self.handle
+
+        mock.mockMethod(handle.DescriptorConfig.readConfig)
+        mock.mockMethod(handle.DescriptorConfig.writeConfig)
+        mock.mockMethod(handle.Targets.createTarget)
+        mock.mockMethod(handle.Targets.configureTargetCredentials)
+        handle.Targets.createTarget._mock.setReturn('target', 'vmware')
+
+        cmd = handle.Commands.getCommandClass('create')()
+
+        err = self.assertRaises(
+            errors.ParseError,
+            cmd.runCommand,
+            handle,
+            {'list': False},
+            ['rbuild', 'create', 'target'],
+            )
+        self.assertEqual(
+            str(err), "'target' missing 1 command parameter(s): TYPE")
+
+        cmd.runCommand(
+            handle,
+            {'list': False},
+            ['rbuild', 'create', 'target', 'vmware'],
+            )
+        handle.DescriptorConfig.readConfig._mock.assertNotCalled()
+        handle.Targets.createTarget._mock.assertCalled('vmware')
+        handle.Targets.configureTargetCredentials\
+            ._mock.assertCalled('target')
+
+        cmd.runCommand(
+            handle,
+            {'list': False, 'from-file': 'foo'},
+            ['rbuild', 'create', 'target', 'vmware'],
+            )
+        handle.DescriptorConfig.readConfig._mock.assertCalled('foo')
+        handle.Targets.createTarget._mock.assertCalled('vmware')
+        handle.Targets.configureTargetCredentials\
+            ._mock.assertCalled('target')
+        handle.DescriptorConfig.writeConfig._mock.assertNotCalled()
+
+        cmd.runCommand(
+            handle,
+            {'list': False, 'to-file': 'foo'},
+            ['rbuild', 'create', 'target', 'vmware'],
+            )
+        handle.DescriptorConfig.readConfig._mock.assertNotCalled()
+        handle.Targets.createTarget._mock.assertCalled('vmware')
+        handle.Targets.configureTargetCredentials\
+            ._mock.assertCalled('target')
+        handle.DescriptorConfig.writeConfig._mock.assertCalled('foo')
+
+
+class ListTargetsTest(AbstractTargetTest):
     def testCommandParsing(self):
-        handle = self.getRbuildHandle()
-        handle.List.registerCommands()
-        handle.Targets.initialize()
-        handle.List.initialize()
+        handle = self.handle
         cmd = handle.Commands.getCommandClass('list')()
 
         mock.mockMethod(handle.Targets.list)
@@ -35,15 +109,14 @@ class ListTargetsTest(rbuildhelp.RbuildHelper):
         handle.Targets.list._mock.assertCalled()
 
     def testCommand(self):
-        self.getRbuildHandle()
         self.checkRbuild('list targets',
             'rbuild_plugins.targets.ListTargetsCommand.runCommand',
             [None, None, {}, ['list', 'targets']])
 
 
-class TargetsPluginTest(rbuildhelp.RbuildHelper):
+class TargetsPluginTest(AbstractTargetTest):
     def testList(self):
-        handle = self.getRbuildHandle()
+        handle = self.handle
 
         mock.mockMethod(handle.facade.rbuilder.getTargets)
 
