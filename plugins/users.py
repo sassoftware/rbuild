6c617ecdfe468b9905ff6c59d7b2fba82217905a
Walter Scheper <Walter.Scheper@sas.com>
2014-05-14 16:29:50 -0400

    Add CreateUserCommand (APPENG-2874,APPENG-2875)
    Adds 'rbuild create user' command and Users.create() method.

--- plugins/users.py
+++ plugins/users.py
@@ -18,7 +18,7 @@
 '''
 users
 '''
-import time
+from xobj import xobj
 
 from rbuild import errors
 from rbuild import pluginapi
@@ -26,7 +26,42 @@ from rbuild.pluginapi import command
 
 
 class CreateUserCommand(command.BaseCommand):
-    pass
+    help = 'Create a new rbuilder user'
+    paramHelp = '[options] <username> <full name> <email>'
+    docs = {
+        'external': 'Use external authentication service',
+        'admin': 'User is an admin',
+        'create-resources': 'User has create resources permissions',
+        'password': 'User password',
+        }
+
+    def addLocalParameters(self, argDef):
+        argDef['external'] = '-e', command.NO_PARAM
+        argDef['admin'] = '-a', command.NO_PARAM
+        argDef['create-resources'] = '-c', command.NO_PARAM
+        argDef['password'] = '-p', command.ONE_PARAM
+
+    def runCommand(self, handle, argSet, args):
+        _, user_name, full_name, email = self.requireParameters(args, expected=[
+                'USERNAME',
+                'FULL_NAME',
+                'EMAIL',
+                ])
+
+        isExternal = argSet.pop('external', False)
+        isAdmin = argSet.pop('admin', False)
+        createResources = argSet.pop('create-resources', False)
+        password = argSet.pop('password', None)
+
+        if password and isExternal:
+            raise errors.BadParameterError("Cannot use external authentication"
+                " and provide a password")
+
+        if not password and not isExternal:
+            password = handle.ui.getPassword('Password')
+
+        handle.Users.create(user_name, full_name, email, password, isExternal,
+            isAdmin, createResources)
 
 
 class DeleteUsersCommand(command.BaseCommand):
@@ -54,11 +89,57 @@ class Users(pluginapi.Plugin):
     name = 'users'
 
     def initialize(self):
-        for command, command_class in (
-                ('list', ListUsersCommand),
+        for command, subcommand, command_class in (
+                ('create', 'user', CreateUserCommand),
+                ('list', 'users', ListUsersCommand),
                 ):
             cmd = self.handle.Commands.getCommandClass(command)
-            cmd.registerSubCommand('users', command_class)
+            cmd.registerSubCommand(subcommand, command_class)
+
+    def create(self, user_name, full_name, email, password, isExternal=False,
+            isAdmin=False, createResources=False):
+        '''
+        Create a rbuilder user
+
+        :param user_name: login name for user
+        :type user_name: str
+        :param full_name: full name of the user
+        :type full_name: str
+        :param email: user's email address
+        :type email: str
+        :param password: user's password, if not using external authentication
+        :type password: str
+        :param isExternal: whether to use external auth, must not be True if
+            password is provided
+        :type isExternal: bool
+        :param isAdmin: is this an admin user
+        :type isAdmin: bool
+        :param createResources: can this user create resources
+        :type createResources: bool
+        '''
+        if isExternal and password:
+            raise errors.PluginError('Cannot use a password with external'
+                ' authentication')
+
+        if not isExternal and not password:
+            raise errors.PluginError('Must provide a password if not using'
+                ' external authentication')
+
+
+        # create the user xml document
+        user_doc = xobj.Document()
+        user_doc.user = user = xobj.XObj()
+        user.user_name = user_name
+        user.full_name = full_name
+        user.password = password
+        user.email = email
+        user.external_auth = isExternal
+        user.is_admin = isAdmin
+        user.can_create = createResources
+
+        # POST the new user
+        client = self.handle.facade.rbuilder._getRbuilderRESTClient()
+        client.api.users.append(user_doc)
 
     def list(self, *args, **kwargs):
         return self.handle.facade.rbuilder.getUsers(**kwargs)
--- rbuild_test/unit_test/pluginstest/userstest.py
+++ rbuild_test/unit_test/pluginstest/userstest.py
@@ -37,6 +37,84 @@ class AbstractUsersTest(rbuildhelp.RbuildHelper):
         self.handle = handle
 
 
+class CreateUserTest(AbstractUsersTest):
+    def testCreateUserArgParse(self):
+        self.checkRbuild(
+            'create user --external --admin --create-resources'
+            ' --password password username "full name" email@example.com',
+            'rbuild_plugins.users.CreateUserCommand.runCommand',
+            [None, None, {
+                'external': True,
+                'admin': True,
+                'create-resources': True,
+                'password': 'password',
+                }, ['create', 'user', 'username', 'full name',
+                    'email@example.com']])
+
+        self.checkRbuild(
+            'create user -e -a -c -p password username "full name"'
+            ' email@example.com',
+            'rbuild_plugins.users.CreateUserCommand.runCommand',
+            [None, None, {
+                'external': True,
+                'admin': True,
+                'create-resources': True,
+                'password': 'password',
+                }, ['create', 'user', 'username', 'full name',
+                    'email@example.com']])
+
+    def testCreateUserCmdline(self):
+        handle = self.handle
+
+        mock.mockMethod(handle.Users.create)
+        mock.mockMethod(handle.ui.getPassword)
+
+        cmd = handle.Commands.getCommandClass('create')()
+
+        err = self.assertRaises(errors.ParseError,
+            cmd.runCommand, handle, {}, ['rbuild', 'create', 'user'])
+        self.assertEqual("'user' missing 3 command parameter(s): USERNAME,"
+            " FULL_NAME, EMAIL", str(err))
+
+        err = self.assertRaises(errors.ParseError,
+            cmd.runCommand, handle, {}, ['rbuild', 'create', 'user', 'foo'])
+        self.assertEqual("'user' missing 2 command parameter(s): FULL_NAME,"
+            " EMAIL", str(err))
+
+        err = self.assertRaises(errors.ParseError,
+            cmd.runCommand, handle, {}, ['rbuild', 'create', 'user', 'foo',
+            '"full name"'])
+        self.assertEqual("'user' missing 1 command parameter(s): EMAIL",
+            str(err))
+
+        err = self.assertRaises(errors.BadParameterError,
+            cmd.runCommand, handle, {'external': True, 'password': 'foo'},
+            ['rbuild', 'create', 'user', 'foo', '"Foo Bar"', 'foo@example.com'])
+        self.assertIn('external authentication', str(err))
+
+        handle.ui.getPassword._mock.setReturn('secret', 'Password')
+        cmd.runCommand(handle, {},
+            ['rbuild', 'create', 'user', 'foo', 'Foo Bar', 'foo@example.com'])
+        handle.Users.create._mock.assertCalled('foo', 'Foo Bar',
+            'foo@example.com', 'secret', False, False, False)
+
+        cmd.runCommand(handle, {'external': True},
+            ['rbuild', 'create', 'user', 'foo', 'Foo Bar', 'foo@example.com'])
+        handle.Users.create._mock.assertCalled('foo', 'Foo Bar',
+            'foo@example.com', None, True, False, False)
+
+        cmd.runCommand(handle, {'external': True, 'admin': True},
+            ['rbuild', 'create', 'user', 'foo', 'Foo Bar', 'foo@example.com'])
+        handle.Users.create._mock.assertCalled('foo', 'Foo Bar',
+            'foo@example.com', None, True, True, False)
+
+        cmd.runCommand(handle,
+            {'external': True, 'admin': True, 'create-resources': True},
+            ['rbuild', 'create', 'user', 'foo', 'Foo Bar', 'foo@example.com'])
+        handle.Users.create._mock.assertCalled('foo', 'Foo Bar',
+            'foo@example.com', None, True, True, True)
+
+
 class ListUsersTest(AbstractUsersTest):
     def testCommand(self):
         self.getRbuildHandle()
@@ -46,3 +124,64 @@ class ListUsersTest(AbstractUsersTest):
         self.checkRbuild('list users 1 2',
             'rbuild_plugins.users.ListUsersCommand.runCommand',
             [None, None, {}, ['list', 'users', '1', '2']])
+
+
+class UsersTest(rbuildhelp.RbuildHelper):
+    def testCreate(self):
+        handle = self.getRbuildHandle()
+        _client = mock.MockObject()
+        mock.mockMethod(handle.facade.rbuilder._getRbuilderRESTClient, _client)
+
+        err = self.assertRaises(errors.PluginError, handle.Users.create, 'foo',
+            'foo bar', 'foo@example.com', 'secret', isExternal=True,
+            isAdmin=True, createResources=True)
+        self.assertIn('external authentication', str(err))
+
+        err = self.assertRaises(errors.PluginError, handle.Users.create, 'foo',
+            'foo bar', 'foo@example.com', '', isExternal=False,
+            isAdmin=True, createResources=True)
+        self.assertIn('Must provide', str(err))
+
+        handle.Users.create('foo', 'foo bar', 'foo@example.com', 'secret',
+            isExternal=False, isAdmin=False, createResources=False)
+        doc = _client.api.users.append._mock.calls[0][0][0]
+        self.assertEqual('foo', doc.user.user_name)
+        self.assertEqual('foo bar', doc.user.full_name)
+        self.assertEqual('foo@example.com', doc.user.email)
+        self.assertEqual('secret', doc.user.password)
+        self.assertFalse(doc.user.external_auth)
+        self.assertFalse(doc.user.is_admin)
+        self.assertFalse(doc.user.can_create)
+
+        handle.Users.create('foo', 'foo bar', 'foo@example.com', None,
+            isExternal=True, isAdmin=False, createResources=False)
+        doc = _client.api.users.append._mock.calls[1][0][0]
+        self.assertEqual('foo', doc.user.user_name)
+        self.assertEqual('foo bar', doc.user.full_name)
+        self.assertEqual('foo@example.com', doc.user.email)
+        self.assertEqual(None, doc.user.password)
+        self.assertTrue(doc.user.external_auth)
+        self.assertFalse(doc.user.is_admin)
+        self.assertFalse(doc.user.can_create)
+
+        handle.Users.create('foo', 'foo bar', 'foo@example.com', None,
+            isExternal=True, isAdmin=True, createResources=False)
+        doc = _client.api.users.append._mock.calls[2][0][0]
+        self.assertEqual('foo', doc.user.user_name)
+        self.assertEqual('foo bar', doc.user.full_name)
+        self.assertEqual('foo@example.com', doc.user.email)
+        self.assertEqual(None, doc.user.password)
+        self.assertTrue(doc.user.external_auth)
+        self.assertTrue(doc.user.is_admin)
+        self.assertFalse(doc.user.can_create)
+
+        handle.Users.create('foo', 'foo bar', 'foo@example.com', None,
+            isExternal=True, isAdmin=True, createResources=True)
+        doc = _client.api.users.append._mock.calls[3][0][0]
+        self.assertEqual('foo', doc.user.user_name)
+        self.assertEqual('foo bar', doc.user.full_name)
+        self.assertEqual('foo@example.com', doc.user.email)
+        self.assertEqual(None, doc.user.password)
+        self.assertTrue(doc.user.external_auth)
+        self.assertTrue(doc.user.is_admin)
+        self.assertTrue(doc.user.can_create)
