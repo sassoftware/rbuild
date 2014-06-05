80813b4dcab6311434fd4f62cea9d11123806b74
Walter Scheper <Walter.Scheper@sas.com>
2014-06-05 14:32:49 -0400

    Fix suggested command line for unconfigured targets (APPENG-2935)
    'rbuild edit target' does not take a target type argument

--- plugins/images.py
+++ plugins/images.py
@@ -155,15 +155,13 @@ class Images(pluginapi.Plugin):
         target = target[0]
         if target.is_configured == 'false':
             raise errors.PluginError(("Target '{0}' is not configured. Try"
-                " running \"rbuild edit target '{0}' '{1}'\" or contacting"
-                " your rbuilder administrator.").format(target.name,
-                        target.target_type.name))
+                " running \"rbuild edit target '{0}'\" or contacting"
+                " your rbuilder administrator.").format(target.name))
 
         if target.credentials_valid == 'false':
             raise errors.PluginError(("Target '{0}' does not have valid"
-                " credentials. Try running \"rbuild edit target '{0}' '{1}'\""
-                " and updating your credentials.").format(target.name,
-                        target.target_type.name))
+                " credentials. Try running \"rbuild edit target '{0}'\""
+                " and updating your credentials.").format(target.name))
 
         image, action = self._getAction(images, target, action_type)
 
