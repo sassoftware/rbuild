77be29321f419b41c15ba97ed4297b1ed14e6f02
Walter Scheper <Walter.Scheper@sas.com>
2014-07-16 16:46:40 -0400

    Refactor imagedef creation
    Move command and code into imagedefs plugin.

--- plugins/imagedefs.py
+++ plugins/imagedefs.py
@@ -18,8 +18,98 @@
 '''
 image definitions
 '''
+import itertools
+
+from xobj import xobj
+
+from rbuild import errors
 from rbuild import pluginapi
 from rbuild.pluginapi import command
+
+
+DESCRIPTOR_PREFIX = 'options.'
+
+DEFERRED = 'deferredImage'
+ISO = 'applianceIsoImage'
+AMI = 'amiImage'
+XEN_OVA = 'xenOvaImage'
+RAW_FS_IMAGE = 'rawFsImage'
+RAW_HD_IMAGE = 'rawHdImage'
+IMAGELESS = 'imageless'
+TARBALL = 'tarballImage'
+VMWARE_ESX = 'vmwareEsxImage'
+VMWARE = 'vmwareImage'
+
+IMAGEDEF_SPECS = {
+    'iso': ISO,
+    'ec2': AMI,
+    'xen': XEN_OVA,
+    'eucalyptus': RAW_FS_IMAGE,
+    'kvm': RAW_HD_IMAGE,
+    'parallels': RAW_HD_IMAGE,
+    'qemu': RAW_HD_IMAGE,
+    'raw_hd': RAW_HD_IMAGE,
+    'layered': DEFERRED,
+    'online_update': IMAGELESS,
+    'tar': TARBALL,
+    'cd': ISO,
+    'dvd': ISO,
+    'esx': VMWARE_ESX,
+    'vcd': VMWARE_ESX,
+    'vmware': VMWARE,
+    'fusion': VMWARE,
+}
+
+
+class CreateImageDefCommand(command.BaseCommand):
+    help = 'Create a image defintion on a SAS App Engine'
+    commands = ['imagedef']
+    paramHelp = '<TYPE> <ARCH>'
+    docs = {'list': 'List available image types',
+            'from-file': 'Load config from file',
+            'to-file': 'Write config to file',
+           }
+
+    def addLocalParameters(self, argDef):
+        argDef['message'] = '-m', command.ONE_PARAM
+        argDef['list'] = '-l', command.NO_PARAM
+        argDef['from-file'] = '-f', command.ONE_PARAM
+        argDef['to-file'] = '-o', command.ONE_PARAM
+
+    def runCommand(self, handle, argSet, args):
+        ui = handle.ui
+
+        message = argSet.pop('message', None)
+        listTypes = argSet.pop('list', False)
+        fromFile = argSet.pop('from-file', None)
+        toFile = argSet.pop('to-file', None)
+
+        if listTypes:
+            ui.write('Available image definition types: %s' %
+                     ', '.join(IMAGEDEF_SPECS))
+            return
+
+        if fromFile:
+            handle.DescriptorConfig.readConfig(fromFile)
+
+        _, selectedType, arch = self.requireParameters(
+            args, expected=['TYPE', 'ARCH'])
+        imageType = IMAGEDEF_SPECS.get(selectedType)
+
+        if not imageType:
+            raise errors.PluginError(
+                "No such image type '%s'. Valid image types are: %s" %
+                (selectedType, ', '.join(sorted(IMAGEDEF_SPECS))))
+
+        if arch not in ['x86', 'x86_64']:
+            raise errors.PluginError(
+                "No such architecture '%s'. Valid architectures are: x86 and"
+                " x86_64" % arch)
+
+        handle.ImageDefs.create(imageType, arch, message)
+
+        if toFile:
+            handle.DescriptorConfig.writeConfig(toFile)
 
 
 class ListImageDefsCommand(command.ListCommand):
@@ -61,7 +151,88 @@ class ListImageDefsCommand(command.ListCommand):
 class ImageDefs(pluginapi.Plugin):
     name = 'imagedefs'
 
+    def _checkBuildDef(self, *args):
+        assert len(args) == 3
+        prodDef = self.handle.product
+
+        if prodDef.platform:
+            templateIter = itertools.chain(
+                prodDef.getBuildTemplates(),
+                prodDef.platform.getBuildTemplates(),
+                )
+        else:
+            # It is valid for the platform to be missing (although not
+            # terribly useful)
+            templateIter = prodDef.getBuildTemplates()
+        allowedCombinations = set(
+            (bt.containerTemplateRef, bt.architectureRef, bt.flavorSetRef)
+            for bt in templateIter)
+        if args not in allowedCombinations:
+            raise errors.PluginError(
+                "Invalid combination of container template, architecture and "
+                "flavor set (%s, %s, %s)" % args)
+        return True
+
+    def create(self, imageType, arch, message):
+        '''
+            Create an image definition
+
+            @param imageType: type of image defintion to create
+            @type imageType: str
+            @return: image defintion
+            @rtype: rObj(...)
+        '''
+        dc = self.handle.DescriptorConfig
+        rb = self.handle.facade.rbuilder
+        pd = self.handle.product
+        ps = self.handle.productStore
+
+        imageTypeDef = rb.getImageTypeDef(imageType, arch)
+        containerRef = imageTypeDef.container.name
+        flavorSetRef = imageTypeDef.flavorSet.name
+        architectureRef = imageTypeDef.architecture.name
+
+        self._checkBuildDef(containerRef, architectureRef, flavorSetRef)
+
+        descriptor = xobj.toxml(imageTypeDef.descriptor._root)
+        ddata = dc.createDescriptorData(fromStream=descriptor)
+
+        imageFields = {}
+        for field in imageTypeDef.options._xobj.attributes:
+            imageFields[field] = getattr(imageTypeDef.options, field)
+
+        for field in ddata.getFields():
+            name = field.getName()
+            if name.startswith(DESCRIPTOR_PREFIX):
+                name = name.replace(DESCRIPTOR_PREFIX, '')
+                imageFields[name] = field.getValue()
+
+        # FIXME: Map allowSnapshots -> vmSnapshots since the smartform and
+        #        proddef differ. (RCE-2743)
+        if 'allowSnapshots' in imageFields:
+            imageFields['vmSnapshots'] = imageFields.pop('allowSnapshots')
+
+        stages = [s.name for s in pd.getStages()]
+
+        pd.addBuildDefinition(
+            name=ddata.getField('displayName'),
+            containerTemplateRef=containerRef,
+            architectureRef=architectureRef,
+            flavorSetRef=flavorSetRef,
+            image=pd.imageType(None, imageFields),
+            stages=stages,
+            )
+        with open(ps.getProductDefinitionXmlPath(), 'w') as fh:
+            pd.serialize(fh, validate=True)
+
+        if message is None:
+            message = 'Add image def %s' % ddata.getField('displayName')
+        ps.commit(message=message)
+        ps.update()
+
     def initialize(self):
+        self.handle.Commands.getCommandClass('create').registerSubCommand(
+            'imagedef', CreateImageDefCommand)
         self.handle.Commands.getCommandClass('list').registerSubCommand(
             'imagedefs', ListImageDefsCommand)
 
--- rbuild_test/unit_test/pluginstest/imagedefstest.py
+++ rbuild_test/unit_test/pluginstest/imagedefstest.py
@@ -20,17 +20,108 @@ from testutils import mock
 from rbuild_test import rbuildhelp
 
 
-class ListImageDefsTest(rbuildhelp.RbuildHelper):
+class AbstractImageDefsTest(rbuildhelp.RbuildHelper):
+    def setUp(self):
+        rbuildhelp.RbuildHelper.setUp(self)
+        self.handle = self.getRbuildHandle()
+        self.handle.Create.registerCommands()
+        self.handle.List.registerCommands()
+        self.handle.ImageDefs.registerCommands()
+        self.handle.Create.initialize()
+        self.handle.List.initialize()
+        self.handle.ImageDefs.initialize()
+
+
+class CreateImageDefTest(AbstractImageDefsTest):
+    def testArgParse(self):
+        self.checkRbuild(
+            'create imagedef --message=foo --list --from-file=fromFile'
+            ' --to-file=toFile type arch',
+            'rbuild_plugins.imagedefs.CreateImageDefCommand.runCommand',
+            [None, None, {
+                'message': 'foo',
+                'list': True,
+                'from-file': 'fromFile',
+                'to-file': 'toFile',
+                }, ['create', 'imagedef', 'type', 'arch']])
+
+    def testCmdLine(self):
+        from rbuild_plugins.imagedefs import IMAGEDEF_SPECS
+        handle = self.handle
+
+        mock.mockMethod(handle.DescriptorConfig.readConfig)
+        mock.mockMethod(handle.DescriptorConfig.writeConfig)
+        mock.mockMethod(handle.ImageDefs.create)
+
+        cmd = handle.Commands.getCommandClass('create')()
+
+        argSet = {}
+        args = ['rbuild', 'create', 'imagedef']
+        err = self.assertRaises(
+            errors.ParseError, cmd.runCommand, handle, argSet, args)
+        self.assertEqual(
+            str(err), "'imagedef' missing 2 command parameter(s): TYPE, ARCH")
+
+        args = ['rbuild', 'create', 'imagedef', 'foo']
+        err = self.assertRaises(
+            errors.ParseError, cmd.runCommand, handle, argSet, args)
+        self.assertEqual(
+            str(err), "'imagedef' missing 1 command parameter(s): ARCH")
+
+        args = ['rbuild', 'create', 'imagedef', 'foo', 'bar']
+        err = self.assertRaises(
+            errors.PluginError, cmd.runCommand, handle, argSet, args)
+        self.assertEqual(
+            str(err), "No such image type 'foo'. Valid image types are: " +
+            ', '.join(sorted(IMAGEDEF_SPECS)))
+
+        args = ['rbuild', 'create', 'imagedef', 'ec2', 'bar']
+        err = self.assertRaises(
+            errors.PluginError, cmd.runCommand, handle, argSet, args)
+        self.assertEqual(
+            str(err), "No such architecture 'bar'. Valid architectures are:"
+            " x86 and x86_64")
+
+        args = ['rbuild', 'create', 'imagedef', 'ec2', 'x86']
+        cmd.runCommand(handle, argSet, args)
+        handle.DescriptorConfig.readConfig._mock.assertNotCalled()
+        handle.ImageDefs.create._mock.assertCalled(
+            'amiImage', 'x86', None)
+        handle.DescriptorConfig.writeConfig._mock.assertNotCalled()
+
+        argSet = {'from-file': 'inFile'}
+        cmd.runCommand(handle, argSet, args)
+        handle.DescriptorConfig.readConfig._mock.assertCalled('inFile')
+        handle.ImageDefs.create._mock.assertCalled(
+            'amiImage', 'x86', None)
+        handle.DescriptorConfig.writeConfig._mock.assertNotCalled()
+
+        argSet = {'from-file': 'inFile', 'to-file': 'outFile'}
+        cmd.runCommand(handle, argSet, args)
+        handle.DescriptorConfig.readConfig._mock.assertCalled('inFile')
+        handle.ImageDefs.create._mock.assertCalled(
+            'amiImage', 'x86', None)
+        handle.DescriptorConfig.writeConfig._mock.assertCalled('outFile')
+
+        argSet = {'from-file': 'inFile', 'to-file': 'outFile',
+                  'message': 'a message'}
+        cmd.runCommand(handle, argSet, args)
+        handle.DescriptorConfig.readConfig._mock.assertCalled('inFile')
+        handle.ImageDefs.create._mock.assertCalled(
+            'amiImage', 'x86', 'a message')
+        handle.DescriptorConfig.writeConfig._mock.assertCalled('outFile')
+
+
+class ListImageDefsTest(AbstractImageDefsTest):
     def testCommand(self):
-        self.getRbuildHandle()
         self.checkRbuild('list imagedefs',
             'rbuild_plugins.imagedefs.ListImageDefsCommand.runCommand',
             [None, None, {}, ['list', 'imagedefs']])
 
 
-class ImageDefsPluginTest(rbuildhelp.RbuildHelper):
+class ImageDefsPluginTest(AbstractImageDefsTest):
     def testList(self):
-        handle = self.getRbuildHandle()
+        handle = self.handle
         mock.mockMethod(handle.facade.rbuilder.getImageDefs)
         mock.mock(handle, 'product')
         mock.mock(handle, 'productStore')
@@ -42,7 +133,7 @@ class ImageDefsPluginTest(rbuildhelp.RbuildHelper):
             product='project', version='branch')
 
     def testListNoProduct(self):
-        handle = self.getRbuildHandle()
+        handle = self.handle
         mock.mockMethod(handle.facade.rbuilder.getImages)
 
         err = self.assertRaises(
